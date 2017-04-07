78c9b8e9a5a211950be2d2fe5cdd32924441d2b4
HChlebek <student@HChlebek.local>
2017-04-07 09:55:12 -0500

    MedicalVC added

---
+++ Essence of Life/MedicalViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MedicalViewController.swift
+//  Essence of Life
+//
+//  Created by HChlebek on 4/7/17.
+//  Copyright © 2017 Les Lentilz. All rights reserved.
+//
+
+import UIKit
+
+class MedicalViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
