e1fe3e61932c89f6e3caed96aafb70f9e4e11ecb
HChlebek <student@hchlebek.local>
2017-04-19 09:50:18 -0500

    new password cell vc

---
+++ Essence of Life/NewPasswordTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  NewPasswordTableViewCell.swift
+//  Essence of Life
+//
+//  Created by HChlebek on 4/19/17.
+//  Copyright © 2017 Les Lentilz. All rights reserved.
+//
+
+import UIKit
+
+class NewPasswordTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var websiteLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
