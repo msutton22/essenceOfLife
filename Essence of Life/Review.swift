d887c3a06c8e1b047234d26ab2eaea7bc82d5db8
Maddy Sutton <student@MSutton.local>
2017-04-20 09:58:56 -0500

    review class eyyyy

--- Essence of Life/Review.swift
+++ Essence of Life/Review.swift
@@ -12,5 +12,58 @@ import os.log
 class Review : NSObject {
     var photo: UIImage?
     var rating: Int = 0
+    var name: String
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("review")
+    
+
+init?(name: String, photo: UIImage?, rating: Int){
+    guard !name.isEmpty else {
+        return nil
+    }
+    
+    guard (rating >= 0) && (rating <= 5) else {
+        return nil
+    }
+    
+    self.name = name
+    self.photo = photo
+    self.rating = rating
     
 }
+
+struct PropertyKey {
+    static let name = "name"
+    static let photo = "photo"
+    static let rating = "rating"
+    }
+
+func encode(with aCoder: NSCoder) {
+    aCoder.encode(name, forKey: PropertyKey.name)
+    aCoder.encode(photo, forKey: PropertyKey.photo)
+    aCoder.encode(rating, forKey: PropertyKey.rating)
+   
+}
+
+
+required convenience init?(coder aDecoder: NSCoder) {
+    guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+        os_log("unable", log: OSLog.default, type: .debug)
+        return nil
+    }
+    
+    
+    let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+    let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+    
+    
+    
+    self.init(name: name, photo: photo, rating: rating)
+    
+}
+
+
+}
+
+
--- FunViewController.swift
+++ FunViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class FunViewController: UIViewController {
+class FunViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var stars: StarControl!
     @IBOutlet weak var imageReview: UIImageView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var reviewTextField: UITextField!
     @IBOutlet weak var reviewText: UITextView!
-
+    var review : Review?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        reviewTextField.delegate = self
+        if let review = review {
+            navigationItem.title = review.name
+            reviewTextField.text = review.name
+            imageReview.image = review.photo
+            stars.rating = review.rating
+        }
+        updateSaveButtonState()
+        
     }
 
     
