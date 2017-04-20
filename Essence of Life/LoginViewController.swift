4b551e1345be06a317813a21972e7ac3b214513b
rkalvani <student@rkalvani.local>
2017-04-20 09:45:53 -0500

    Adjusted Storyboard and created new swift file for the touch Id set up

---
+++ Essence of Life/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Essence of Life
+//
+//  Created by rkalvani on 4/20/17.
+//  Copyright © 2017 Les Lentilz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func loginAction(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
