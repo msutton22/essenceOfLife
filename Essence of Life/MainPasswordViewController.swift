7306b3188e718accc3bbdccab511d9a970754d03
HChlebek <student@hchlebek.local>
2017-04-21 09:44:46 -0500

    changes to password

--- Essence of Life/MainPasswordViewController.swift
+++ Essence of Life/MainPasswordViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class MainPasswordViewController: UIViewController
+class MainPasswordViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
 
     @IBOutlet weak var addButton: UIBarButtonItem!
+    @IBOutlet weak var passwordTableView: UITableView!
     
     var passwords : [Password] = []
     
@@ -19,9 +20,24 @@ class MainPasswordViewController: UIViewController
     {
         super.viewDidLoad()
 
-        
+        passwords.append(Password(Website: "twitter", Username: "123456", Password: "78970"))
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "passwordCell", for: indexPath) as! NewPasswordTableViewCell
+        cell1.websiteLabel.text = passwords[indexPath.row].website
+        cell1.usernameLabel.text = passwords[indexPath.row].username
+        cell1.passwordLabel.text = passwords[indexPath.row].password
+        
+        return cell1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return passwords.count
+    }
+    
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem)
     {
         
