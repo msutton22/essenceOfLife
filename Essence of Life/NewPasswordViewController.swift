ca7f888172a9fb841953f5e2fbffaf356a37773c
HChlebek <student@hchlebek.local>
2017-04-26 09:47:55 -0500

    vnjfdnvdufn

--- Essence of Life/NewPasswordViewController.swift
+++ Essence of Life/NewPasswordViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 
 class NewPasswordViewController: UIViewController
 {
@@ -14,7 +15,9 @@ class NewPasswordViewController: UIViewController
     @IBOutlet weak var websiteTextfield: UITextField!
     @IBOutlet weak var usernameTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    var myPasswords : Password?
     
     
     override func viewDidLoad()
@@ -44,6 +47,24 @@ class NewPasswordViewController: UIViewController
         
     }
     
-    
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard let button = sender as? UIBarButtonItem, button == saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        myPasswords?.website = websiteTextfield.text!
+        myPasswords?.username = usernameTextfield.text!
+        myPasswords?.password = passwordTextfield.text!
+    }
+    private func updateSaveButtonState()
+    {
+        let text1 = websiteTextfield.text ?? ""
+        saveButton.isEnabled = !text1.isEmpty
+        let text2 = usernameTextfield.text ?? ""
+        saveButton.isEnabled = !text2.isEmpty
+        let text3 = passwordTextfield.text ?? ""
+        saveButton.isEnabled = !text3.isEmpty
+    }
 }
