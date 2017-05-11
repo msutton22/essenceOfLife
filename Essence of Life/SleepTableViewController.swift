e8391621f650e156069ac12250ccc7f1a71ad4f6
HChlebek <student@hchlebek.local>
2017-05-11 09:37:37 -0500

    working on sleep VC

---
+++ Essence of Life/SleepTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SleepTableViewController.swift
+//  Essence of Life
+//
+//  Created by HChlebek on 5/11/17.
+//  Copyright © 2017 Les Lentilz. All rights reserved.
+//
+
+import UIKit
+
+class SleepTableViewController: UITableViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+   
+
+}
