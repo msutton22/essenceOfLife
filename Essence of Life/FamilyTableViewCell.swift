187976cab66fbe1f3084899bde3c2219481b9211
HChlebek <student@hchlebek.local>
2017-05-15 10:05:25 -0500

    working on family

---
+++ Essence of Life/FamilyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FamilyTableViewCell.swift
+//  Essence of Life
+//
+//  Created by HChlebek on 5/15/17.
+//  Copyright © 2017 Les Lentilz. All rights reserved.
+//
+
+import UIKit
+
+class FamilyTableViewCell: UITableViewCell
+{
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
